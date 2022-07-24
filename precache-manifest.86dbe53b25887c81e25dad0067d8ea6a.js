55a0b82b31269324feaaf9cf68e63b293ac71617
vishal katkar <vgkatkar20@gmail.com>
2022-07-24 15:12:35 +0530

    Updates

---
+++ precache-manifest.86dbe53b25887c81e25dad0067d8ea6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebe75ffe432c29438c0",
+    "url": "/Todos-List/static/css/main.bc2a76e4.chunk.css"
+  },
+  {
+    "revision": "9ebe75ffe432c29438c0",
+    "url": "/Todos-List/static/js/main.9ebe75ff.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/Todos-List/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "7293154af6b8ccf2798a",
+    "url": "/Todos-List/static/js/2.7293154a.chunk.js"
+  },
+  {
+    "revision": "5182e4950483036eeb51",
+    "url": "/Todos-List/static/js/runtime~main.5182e495.js"
+  },
+  {
+    "revision": "6fe91156010957f27fb7b0a7d029d192",
+    "url": "/Todos-List/index.html"
+  }
+];
